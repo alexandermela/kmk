9a96f881e1f1442dcf3ad014a33e89ea4fc520cf
alexandermela <96301020+alexandermela@users.noreply.github.com>
2021-12-18 15:03:16 +0630

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,16 @@
+from time import sleep
+import os
+
+os.system("clear")
+a = input('hello! type your full name please >>> ')
+b = ("fuck")
+
+if a == b:
+	print("hello! how are you master")
+else:
+	os.system("figlet fuck | lolcat")
+	os.system("figlet you | lolcat")
+	print(a)
+
+sleep (3)
+os.system("clear")
---
+++ main.sh
@@ -0,0 +1,19 @@
+pkg update -y
+pkg upgrade -y
+termu-setup-storage
+pkg install figlet -y
+pkg install python -y
+pip install pyfiglet
+apt-get install ruby -y
+pip install lolcat
+pip install clock
+gem install lolcat -y
+pkg install sleep
+
+python sleep1s.py
+clear
+
+figlet Hello | lolcat
+echo "Mr.Kaung Min Khant" | lolcat
+python sleep1s.py
+python 1.py
